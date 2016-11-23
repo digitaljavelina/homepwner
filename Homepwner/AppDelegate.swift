f53466e7ed54c12b64d8a1181d2ad9f367f8ac13
Michael Henry <mike@digitaljavelina.com>
2016-11-22 18:24:38 -0700

    implemented writing to the filesystem with NSData

--- Homepwner/AppDelegate.swift
+++ Homepwner/AppDelegate.swift
@@ -12,13 +12,13 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    // create an item store
+    let itemStore = ItemStore()
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        // create an ItemStore
-        let itemStore = ItemStore()
         
         // create an ImageStore
         let imageStore = ImageStore()
@@ -38,8 +38,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        let success = itemStore.saveChanges()
+        if success {
+            print("Saved all of the Items")
+        } else {
+            print("Could not sace any of the Items")
+        }
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
